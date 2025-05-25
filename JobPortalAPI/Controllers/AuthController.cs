8977d11ec0097ef81b5d76136ef15ba34fcc6d90
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-05-25 11:57:32 +0500

    FEAT: add AuthController with register and login using DTOs

---
+++ JobPortalAPI/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using JobPortalAPI.Data;
+using JobPortalAPI.DTOs;
+using JobPortalAPI.Models;
+using JobPortalAPI.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobPortalAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IPasswordHasher<User> _hasher;
+        private readonly JwtService _jwt;
+        
+        public AuthController(ApplicationDbContext db, IPasswordHasher<User> hasher, JwtService jwt)
+        {
+            _db = db;
+            _hasher = hasher;
+            _jwt = jwt;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            if (await _db.Users.AnyAsync(u => u.Email == dto.Email))
+                return BadRequest("This email is on the list");
+
+            var user = new User
+            {
+                FullName = dto.FullName,
+                Email = dto.Email,
+            };
+
+            user.PasswordHash = _hasher.HashPassword(user, dto.Password);
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
+
+            var token = _jwt.GenerateToken(user);
+            return Ok(new { token });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var user = await _db.Users.FirstOrDefaultAsync(u => u.Email == dto.Email);
+            if (user == null)
+                return Unauthorized("User not found");
+
+            var result = _hasher.VerifyHashedPassword(user, user.PasswordHash!, dto.Password);
+            if (result == PasswordVerificationResult.Failed)
+                return Unauthorized("Incorrect password");
+
+            var token = _jwt.GenerateToken(user);
+            return Ok(new { token });
+        }
+    }
+}
