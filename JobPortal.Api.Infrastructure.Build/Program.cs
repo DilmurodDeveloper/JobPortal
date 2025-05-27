7bfdbc71a4a252e26c23767e081a6870091a7404
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-05-27 14:16:23 +0500

    FEAT: Add GitHub Actions CI pipeline

---
+++ JobPortal.Api.Infrastructure.Build/Program.cs
@@ -0,0 +1,74 @@
+﻿using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var aDotNetClient = new ADotNetClient();
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "JobPortal Build Pipeline",
+
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new[] { "main" }
+        },
+        Push = new PushEvent
+        {
+            Branches = new[] { "main" }
+        }
+    },
+
+    Jobs = new Dictionary<string, Job>
+    {
+        {
+            "Build",
+            new Job
+            {
+                RunsOn = BuildMachines.Windows2022,
+
+                Steps = new List<GithubTask>
+                {
+                    new CheckoutTaskV2
+                    {
+                        Name = "Checking Out Code"
+                    },
+                    new SetupDotNetTaskV1
+                    {
+                        Name = "Setting Up .NET",
+                        TargetDotNetVersion = new TargetDotNetVersion
+                        {
+                            DotNetVersion = "8.0.x"
+                        }
+                    },
+                    new RestoreTask
+                    {
+                        Name = "Restoring Packages"
+                    },
+                    new DotNetBuildTask
+                    {
+                        Name = "Building Project"
+                    },
+                    new TestTask
+                    {
+                        Name = "Running Tests"
+                    }
+                }
+            }
+        }
+    }
+};
+
+string buildScriptPath = Path.Combine(Directory
+    .GetCurrentDirectory(), "..", "..", "..", ".github", "workflows", "dotnet.yml");
+
+string? directoryPath = Path.GetDirectoryName(buildScriptPath);
+
+if (!Directory.Exists(directoryPath))
+{
+    Directory.CreateDirectory(directoryPath!);
+}
+
+aDotNetClient.SerializeAndWriteToFile(githubPipeline, path: buildScriptPath);
